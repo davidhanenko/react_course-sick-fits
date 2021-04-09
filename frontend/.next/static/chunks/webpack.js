083cc85a75e2365075b96b7b8b8ba7fce3fb5866
davidhanenko <d.hanenko24@gmail.com>
2021-04-08 23:47:08 -0400

    nextjs boilerplate

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d498a49ca65f25cb0d00";
+/******/ 	var hotCurrentHash = "94710d2158269ac859de";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
