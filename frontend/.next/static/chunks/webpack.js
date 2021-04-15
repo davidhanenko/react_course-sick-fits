5d4eb49698bb866bc7b07fbad05c1f05c50cfcc1
davidhanenko <d.hanenko24@gmail.com>
2021-04-14 22:15:02 -0400

    apollo setup

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ec8f076b8e85f813095d";
+/******/ 	var hotCurrentHash = "9db81233f9df6f090210";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
