48559b131ee3ff72903dab832bdcc4da252736f9
davidhanenko <d.hanenko24@gmail.com>
2021-06-08 23:14:51 -0400

    Checkout handle logic on client

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a6edfd19c63b805e7250";
+/******/ 	var hotCurrentHash = "7d1c6ebe3275fbd5a4f6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
